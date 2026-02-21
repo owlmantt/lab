14949cafbadb06f70c99a3098e734ce4d5ac0124
owlmantt <atyrau25051955@gmail.com>
2026-02-21 09:25:48 +0500

    feat: complete search app implementation and fix types

---
+++ lab-06/task1/src/pages/Courses.tsx
@@ -0,0 +1,14 @@
+const Courses = () => {
+  return (
+    <div>
+      <h2>Available Courses</h2>
+      <ul>
+        <li>React Architecture</li>
+        <li>TypeScript Masters</li>
+        <li>Backend with Node</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Courses;
