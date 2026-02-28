3d21c3369fc6ef88bce3050cd75c9a34e570d006
owlmantt <atyrau25051955@gmail.com>
2026-02-28 09:12:17 +0500

    feat: complete search app implementation and fix types

---
+++ lab-07/task1/src/components/LoadingSpinner.tsx
@@ -0,0 +1,8 @@
+export default function LoadingSpinner() {
+    return(
+        <div className="spinner-conainer">
+        <div className="loading-spinner"></div>
+        <p>Loading</p>
+    </div>
+    );
+}
